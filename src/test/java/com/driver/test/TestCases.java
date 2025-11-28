a88809081cd5cbf9e7b6cfed56276c7170dab9a5
Prapitesh <aitipamulaprapitesh02@gmail.com>
2025-11-28 13:56:04 +0530

    method overriding task done

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -2,7 +2,25 @@ package com.driver.test;
 
 import static org.junit.Assert.*;
 import org.junit.jupiter.api.Test;
+import com.driver.*;
 
 public class TestCases {
-    
+
+    @Test
+    public void testParentMethod(){
+        A obj = new A();
+        assertEquals("Invoking method from class A", obj.meth());
+    }
+
+    @Test
+    public void testOverriddenMethod(){
+        A obj = new B(); // parent reference holding child object
+        assertEquals("Method is overridden in Extended class B", obj.meth());
+    }
+
+    @Test
+    public void testDirectChildCall(){
+        B obj = new B();
+        assertEquals("Method is overridden in Extended class B", obj.meth());
+    }
 }
